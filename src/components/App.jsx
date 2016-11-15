7fca31713d88218cf353a686de5f1856bfbd2739
j- <j@skeoh.com>
2016-11-15 14:56:10 +1100

    Render operation demo list to application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 const React = require('react');
+const OperationDemoList = require('./OperationDemoList');
 
 const App = () => (
 	<div>
 		<h1>Global Composite Operation</h1>
+		<OperationDemoList />
 	</div>
 );
 
