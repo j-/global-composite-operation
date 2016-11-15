99ae066c4ff9599fdfb552ca7bcb0f9884e4d726
j- <j@skeoh.com>
2016-11-15 16:04:50 +1100

    Pass NODE_ENV through to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const DefinePlugin = webpack.DefinePlugin;
 const HTMLPlugin = require('html-webpack-plugin');
 
 const resolve = (relative) => path.resolve(__dirname, relative);
@@ -26,6 +28,11 @@ module.exports = {
 	plugins: [
 		new HTMLPlugin({
 			template: resolve('./src/index.html'),
+		}),
+		new DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+			},
 		}),
 	],
 	devtool: 'source-map',
