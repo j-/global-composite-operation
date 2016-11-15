2eff5cd852e3ea0b427c51268a34d97e38bab811
j- <j@skeoh.com>
2016-11-15 14:58:51 +1100

    Briefly explain the rendering process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ const OperationDemoList = require('./OperationDemoList');
 const App = () => (
 	<div>
 		<h1>Global Composite Operation</h1>
+		<p>In each canvas below, the red 'destination' square is rendered first,
+		then the blue 'source' square.</p>
 		<OperationDemoList />
 	</div>
 );
